c58e056dee739e23a1d5a8ac9eb5ce53377ec47b
stef663k <stef663k@elev.techcollege.dk>
2023-06-27 14:18:45 +0200

    ConnectionString

--- ApiProject/StartProjectApi/Model/Models.cs
+++ ApiProject/StartProjectApi/Model/Models.cs
@@ -5,6 +5,7 @@ public class Models{
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id{ get; set; }
-    public string? Name { get; set; }
+    [BsonElement("Name")]
+    public string? PlayerName { get; set; }
     public int? Score{ get; set; }
 }
